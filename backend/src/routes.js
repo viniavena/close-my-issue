728419ba2d61252bea0f7edc14e9153ade48a081
Vini Avena <admin@admins-MacBook-Air.local>
2021-07-08 02:13:43 -0300

    create & list openers account routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/projects', (req, res) => {
-  const body = req.body;
-  console.log(body);
-  return res.json({ project: 'close my issue', author: 'Vinicius Avena' });
+// get the list of openers account
+routes.get('/openers', async (req, res) => {
+  const openers = await connection('openers').select('*');
+
+  return res.json(openers);
+});
+
+// creat an opener account { username, email }
+routes.post('/openers', async (req, res) => {
+  const { username, email } = req.body;
+
+  const id = crypto.randomBytes(2).toString('HEX');
+
+  await connection('openers').insert({
+    id,
+    username,
+    email,
+  });
+
+  return res.json({ id });
 });
 
 module.exports = routes;
