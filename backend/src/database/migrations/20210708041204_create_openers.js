4582df1e9550410a83cc2c38b39088b449d0f4ac
Vini Avena <admin@admins-MacBook-Air.local>
2021-07-08 01:36:05 -0300

    openers migration

--- backend/src/database/migrations/20210708041204_create_openers.js
+++ backend/src/database/migrations/20210708041204_create_openers.js
@@ -1,9 +1,13 @@
+// npx knex migrate:make create_openers
+
 exports.up = function (knex) {
-  knex.schema.createTable('openers', function (table) {
+  return knex.schema.createTable('openers', function (table) {
     table.string('id').primary();
     table.string('username').notNullable();
     table.string('email').notNullable();
   });
 };
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable('openers');
+};
